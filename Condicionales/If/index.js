5c9eccc889d73446481d7b2aca595533bc1b7086
Pablo <144966084+Pablobaji@users.noreply.github.com>
2023-10-09 18:46:19 -0600

    Add files via upload

---
+++ Condicionales/If/index.js
@@ -0,0 +1,7 @@
+let edad='20';
+console.log("Tu edad es: "+edad)
+if (edad=>18){
+    console.log ("Eres mayor de edad");
+}else{
+    console.log ("Ers menor de edad");
+}
---
+++ Condicionales/Switch/index.js
@@ -0,0 +1,7 @@
+let producto = "Naranja";
+console.log ("Tu producto es: "+producto)
+switch (producto){
+    case "Naranja":
+        console.log ("Las naranjas cuestan 10$");
+        break;
+}
